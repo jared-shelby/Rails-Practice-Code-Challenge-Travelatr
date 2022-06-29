91547082665c3ad89466ca1101949b9a81cb354f
Jared Shelby <jared.shelby@yale.edu>
2022-06-29 09:39:24 -0400

    validations & blogger instance methods added

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,4 +1,23 @@
+## BLOGGER MODEL
 class Blogger < ApplicationRecord
+    
+    # associations
     has_many :posts
     has_many :destinations, through: :posts
+
+    # validations
+    validates :name, presence: true, uniqueness: true
+    validates :age, numericality: { only_integer: true, greater_than: 0 }
+    validates :bio, length: { minimum: 31 }
+
+    # return total likes on all posts
+    def total_likes
+        self.posts.sum { |post| post.likes }
+    end
+
+    # return post with most likes
+    def featured_post
+        self.posts.max_by { |post| post.likes }
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
+## POST MODEL
 class Post < ApplicationRecord
+    
+    # associations
     belongs_to :blogger
     belongs_to :destination
+
+    # validations
+    validates :title, presence: true
+    validates :content, length: { minimum: 101 }
+
 end
